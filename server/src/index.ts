12aab345b0005a595d5120724bfe54af03ac9ff5
SaurabhMagara <saurabhmagara@gmail.com>
2024-12-17 13:07:52 +0530

    final change

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,11 +10,7 @@ const app = express();
 
 // Allow access origin for requests from frontend
 app.use(cors({
-<<<<<<< HEAD
-    origin: process.env.ORIGIN,
-=======
     origin: process.env.ORIGIN, // This is used to specify allowed CORS origin
->>>>>>> aba0418edfc2cf0adce52cbf9241522ea98684ba
     credentials: true
 }));
 
@@ -27,19 +23,9 @@ app.use(cookieParser());
 // API Routes
 // app.use("/api/v1", allRoutes);
 
-<<<<<<< HEAD
+
 app.listen(process.env.PORT || 5001,()=>{
     console.log("Port is running :", process.env.PORT);
 });
-=======
-// Vercel assigns a port automatically, use process.env.PORT
-// const port = process.env.PORT || 5001;
-// app.listen(port, () => {
-//     console.log(`Server is running on port: ${port}`);
-// });
-app.get("/", (req :express.Request, res:express.Response) => {
-    res.send("Hello from Vercel!");
-});
 
 export default app;
->>>>>>> aba0418edfc2cf0adce52cbf9241522ea98684ba
--- server/src/middleware/refreshAccessToken.middleware.ts
+++ server/src/middleware/refreshAccessToken.middleware.ts
@@ -12,38 +12,31 @@ export const refreshAccessToken = async (req: express.Request, res: express.Resp
         const token = req.cookies; //conditional check
 
         //if there is token then call next
-        if (token) {            
+        if (token) {
             return next();
-        } 
-            // if there is not token then 
-            // using URLSearchParams for giving url encoded form data
-<<<<<<< HEAD
-            const response = await axios.post("https://accounts.spotify.com/api/token",{
-                grant_type: "client_credentials",
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET
-=======
-            const response = await axios.post("https://accounts.spotify.com/api/token", {
-                grant_type : "client_credentials",
-                client_id : client_id,
-                client_secret : client_secret
->>>>>>> aba0418edfc2cf0adce52cbf9241522ea98684ba
-            },
+        }
+        // if there is not token then 
+        // using URLSearchParams for giving url encoded form data
+        const response = await axios.post("https://accounts.spotify.com/api/token", {
+            grant_type: "client_credentials",
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET
+        }
                 {
-                    headers: {
-                        "Content-Type": "application/x-www-form-urlencoded"
-                    }
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
                 }
-            );
+            }
+        );
 
-            console.log("resfreshing token...");
-            
+        console.log("resfreshing token...");
 
-            //sending response
-            res
-                .status(200)
-                .cookie("token", response.data.access_token, { httpOnly:true, maxAge: 58*60*1000 }) // max age defines expiry
-                .json({ message: "token refreshed", AccessToken: token });
+
+        //sending response
+        res
+            .status(200)
+            .cookie("token", response.data.access_token, { httpOnly: true, maxAge: 58 * 60 * 1000 }) // max age defines expiry
+            .json({ message: "token refreshed", AccessToken: token });
     } catch (error) {
         console.log(error);
         // if res dosent send header then gives error 
