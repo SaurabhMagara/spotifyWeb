3ec5b2e2f2ee73ebc6cc1b99d927776caaa25a57
SaurabhMagara <saurabhmagara@gmail.com>
2024-12-17 13:38:08 +0530

    fixed

--- server/src/middleware/refreshAccessToken.middleware.ts
+++ server/src/middleware/refreshAccessToken.middleware.ts
@@ -21,7 +21,7 @@ export const refreshAccessToken = async (req: express.Request, res: express.Resp
             grant_type: "client_credentials",
             client_id: process.env.CLIENT_ID,
             client_secret: process.env.CLIENT_SECRET
-        }
+        },
                 {
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
